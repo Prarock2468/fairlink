dd4f919d47985c87ac114de3f0b59f1b2746cf0a
Nitin K Sharma <nk32209@gmail.com>
2024-06-26 18:06:22 +0530

    page responsive

--- js/dining.js
+++ js/dining.js
@@ -2,6 +2,11 @@ var swiperding = new Swiper('.dining_swiper_container', {
     loop:true,
     slidesPerView: 2,
     spaceBetween: 0,
+    navigation: {
+        nextEl: ".swiper-dining-prev",
+        prevEl: ".swiper-dining-next",
+      },
+      parallax:true,
     breakpoints: {
         1920: {
             slidesPerView: 2,
@@ -9,6 +14,10 @@ var swiperding = new Swiper('.dining_swiper_container', {
         },
         1028: {
             slidesPerView: 2,
+            spaceBetween: 0
+        },
+        769:{
+            slidesPerView: 1,
             spaceBetween: 0
         },
         480: {
@@ -24,6 +33,10 @@ var swiperoffer = new Swiper('.offer_swiper_container', {
     loop:true,
     slidesPerView: 1,
     spaceBetween: 0,
+    navigation: {
+        nextEl: ".swiper-dining-next",
+        prevEl: ".swiper-dining-prev",
+      },
     breakpoints: {
         1920: {
             slidesPerView: 1,
